508171c2c39ab6199baf2c926a0bc508b9db8598
tasnim-jawad <muhammadtasnimulhasan1@gmail.com>
2021-09-01 08:52:21 +0600

    overlay

--- js/custom.js
+++ js/custom.js
@@ -4,6 +4,15 @@ for (let index = 1; index < 9; index++) {
     html +=`
     <div class="col-3 px-0" >
         <div class="gallery_image">
+            <div class="overlay">
+                <div class="eye_circle">
+                    <a href="#">
+                        <i class="far fa-eye"></i>
+                    </a>
+                </div>
+                <h3>Labore et dolore magnam</h3>
+                <p>Wordpress</p>
+            </div>
             <img src="img/galleryimg${index}.png" alt="">
         </div>
     </div>
